3c6460ef4b1713f25d695ef97602982b32d82d5e
R Sri Sudarshan <103353218+srisudarshanrg@users.noreply.github.com>
2022-08-06 17:25:05 +0530

    Autobiographical Website

---
+++ functions/index.js
@@ -0,0 +1,9 @@
+let askName = prompt("Hey There! What Is Your Good Name?")
+
+if (askName == "Sudarshan") {
+    alert("You must be the owner of this website!!")
+}
+
+else {
+    alert("Hello " + askName + "! Nice To Meet You!")
+}
